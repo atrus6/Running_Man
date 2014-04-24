713a9f11e1e3cfe11b1fcd87d8e0136f192dfded
Tim Butram <tbutram@worldsproject.org>
2014-04-23 21:47:26 -0400

    Added a better menu with some more options.

--- core/src/org/worldsproject/game/runningman/MenuScreen.java
+++ core/src/org/worldsproject/game/runningman/MenuScreen.java
@@ -2,8 +2,12 @@ package org.worldsproject.game.runningman;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 /**
  * Created by atrus on 4/17/14.
@@ -11,13 +15,80 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 public class MenuScreen implements Screen {
     final RunningMan game;
 
-    OrthographicCamera camera;
+    private OrthographicCamera camera;
+
+    //UI Elements
+    private Skin skin;
+    private Stage stage;
 
     public MenuScreen(final RunningMan game) {
         this.game = game;
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
+
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        createUI();
+    }
+
+    private void createUI() {
+        // A skin can be loaded via JSON or defined programmatically, either is fine. Using a skin is optional but strongly
+        // recommended solely for the convenience of getting a texture, region, etc as a drawable, tinted drawable, etc.
+        skin = new Skin();
+
+        // Generate a 1x1 white texture and store it in the skin named "white".
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+        skin.add("white", new Texture(pixmap));
+
+        // Store the default libgdx font under the name "default".
+        BitmapFont font = new BitmapFont();
+        skin.add("default", font);
+
+        // Configure a TextButtonStyle and name it "default". Skin resources are stored by type, so this doesn't overwrite the font.
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+        textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+        textButtonStyle.font = skin.getFont("default");
+
+        skin.add("default", textButtonStyle);
+
+        // Create a table that fills the screen. Everything else will go inside this table.
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        // Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than "default".
+        final TextButton button = new TextButton("Click me!", skin);
+        table.add(button);
+
+        // Add a listener to the button. ChangeListener is fired when the button's checked state changes, eg when clicked,
+        // Button#setChecked() is called, via a key press, etc. If the event.cancel() is called, the checked state will be reverted.
+        // ClickListener could have been used, but would only fire when clicked. Also, canceling a ClickListener event won't
+        // revert the checked state.
+        button.addListener(new ChangeListener() {
+            public void changed (ChangeEvent event, Actor actor) {
+                game.setScreen(new GameScreen(game));
+                dispose();
+            }
+        });
+
+        // Add an image actor. Have to set the size, else it would be the size of the drawable (which is the 1x1 texture).
+        table.add(new Image(skin.newDrawable("white", Color.RED))).size(64);
+        table.debug();
+        table.left();
+
+        VerticalGroup vg = new VerticalGroup();
+        vg.addActor(new TextButton("Highscores", skin));
+        vg.space(50);
+        vg.addActor(new TextButton("Credits", skin));
+
+        table.add(vg);
     }
 
     @Override
@@ -28,20 +99,14 @@ public class MenuScreen implements Screen {
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
 
-        game.batch.begin();
-        game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
-        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
-        game.batch.end();
-
-        if (Gdx.input.isTouched()) {
-            game.setScreen(new GameScreen(game));
-            dispose();
-        }
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
+        Table.drawDebug(stage);
     }
 
     @Override
     public void resize(int width, int height) {
-
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
@@ -66,6 +131,7 @@ public class MenuScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
+        skin.dispose();
     }
 }
